011ff5f4c0ece58a09fe460607e26b033efa47d7
radhikavarshney <rvarshney2001@gmail.com>
2022-11-05 12:55:56 +0530

    finished the rock paper scissors game

--- Rock Paper Scissors/app.js
+++ Rock Paper Scissors/app.js
@@ -1,4 +1,68 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice= document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay= document.getElementById('user-choice')
 const resultDisplay = document.getElementById('Result')
-const possibleChoices = document.getElementById('')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener('click',(e)=>{userChoice=e.target.id
+userChoiceDisplay.innerHTML = userChoice
+genrateComputerChoice()
+getResult()
+}))
+
+function genrateComputerChoice()
+{
+    const randomNumber = Math.floor(Math.random()*3)+1
+    
+    if (randomNumber ===1)
+    {
+       computerChoice='rock'
+    }
+    if (randomNumber ===2)
+    {
+       computerChoice='paper'
+    }
+    if (randomNumber ===3)
+    {
+       computerChoice='scissors'
+    }
+    computerChoiceDisplay.innerHTML=computerChoice
+}
+
+function getResult()
+{
+    if (computerChoice===userChoice)
+    {
+        result='its a draw!'
+    }
+    if (computerChoice=== 'rock' && userChoice==="paper")
+    {
+        result='you win!'
+    }
+    if (computerChoice=== 'rock' && userChoice==='scissors')
+    {
+        result='you lost!'
+    }
+    if (computerChoice=== 'scissors' && userChoice==="paper")
+    {
+        result='you lost!'
+    }
+    if (computerChoice=== 'scissors' && userChoice==="rock")
+    {
+        result='you win!'
+    }
+    if (computerChoice=== 'paper' && userChoice==="rock")
+    {
+        result='you lost!'
+    }
+    if (computerChoice=== 'paper' && userChoice==="scissors")
+    {
+        result='you win!'
+    }
+
+
+     resultDisplay.innerHTML=result
+
+}
